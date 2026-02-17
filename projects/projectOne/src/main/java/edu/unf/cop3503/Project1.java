bac585347cfbaf08694c397f53f9d5d43173f940
Albert Simpson <albertsimpson00@gmail.com>
2026-02-17 16:54:00 -0500

    Initialized project
    Deleted Hello World print
    added scanner and read name helper method.

--- projects/projectOne/src/main/java/edu/unf/cop3503/Project1.java
+++ projects/projectOne/src/main/java/edu/unf/cop3503/Project1.java
@@ -1,7 +1,27 @@
 package edu.unf.cop3503;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Project1 {
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner scnr = new Scanner(System.in);
+
+        String[] names = readNames(scnr);
+
     }
+
+    public static String[] readNames(Scanner scnr) {
+        System.out.println("Enter List of Names Separated by Commas");
+        String inputLine = scnr.nextLine();
+
+        String[] names = inputLine.split(","); // splits inputLine into an array names
+        for (int i = 0; i < names.length; i++) { // Iterate over each name
+            names[i] = names[i].trim(); // Trim white spaces from each iteration/name
+        }
+        return names;
+    }
+
+
 }
