9d36e89e1a013b9183fd903ebda3925973d6d685
Albert Simpson <albertsimpson00@gmail.com>
2026-02-17 16:56:49 -0500

    Added test ArrayList<String> type method
    Test/draft code for StringBuilder & ArrayList<String> usage

--- projects/projectOne/src/main/java/edu/unf/cop3503/Project1.java
+++ projects/projectOne/src/main/java/edu/unf/cop3503/Project1.java
@@ -5,6 +5,28 @@ import java.util.Scanner;
 
 public class Project1 {
 
+    /*public static ArrayList<String> newNamesList(String names) {
+        ArrayList<String> nameArray = new ArrayList<>();
+        StringBuilder temp = new StringBuilder();
+        char curr = ' ';
+        String fullName = temp.toString();
+
+        for(int i = 0; i < names.length(); i++) {
+            curr = names.charAt(i);
+            if (curr == ',') {
+                fullName = fullName.trim();
+                nameArray.add(String.valueOf(fullName)); // Convert StringBuilder to type String for namesArray
+                temp.setLength(0); // Reset StringBuilder
+            } else {
+                temp.append(curr);
+            }
+        }
+        nameArray.add(String.valueOf(fullName)); // Add remaining inside temp since last name does not end with "comma"
+        return nameArray;
+    }
+    */
+
+
     public static void main(String[] args) {
         Scanner scnr = new Scanner(System.in);
 
@@ -22,6 +44,5 @@ public class Project1 {
         }
         return names;
     }
-
 
 }
