f4f6e6d2c9c58687dae5429425cf97a443501502
Albert Simpson <albertsimpson00@gmail.com>
2026-03-20 22:06:05 -0400

    Finished projectTwo draft
    maybe code is ready for submission

--- projects/projectTwo/src/main/java/unf/edu/cop3503/RoadSection.java
+++ projects/projectTwo/src/main/java/unf/edu/cop3503/RoadSection.java
@@ -1,5 +1,9 @@
 package unf.edu.cop3503;
 
+/**
+ * Stores matching road volume and speed data for a single date and time,
+ * along with calculated total and average values.
+ */
 public class RoadSection {
     private RoadVolume roadVolume;
     private RoadSpeed roadSpeed;
@@ -8,26 +12,50 @@ public class RoadSection {
     private double volumeAvg;
     private double speedAvg;
 
+    /**
+     * Creates a RoadSection object using matching volume and speed objects.
+     * Derived values are calculated when the object is created.
+     *
+     * @param roadVolume the matching road volume object
+     * @param roadSpeed the matching road speed object
+     */
     public RoadSection(RoadVolume roadVolume, RoadSpeed roadSpeed) {
+
+        // Stores the matching volume and speed objects
         this.roadVolume = roadVolume;
         this.roadSpeed = roadSpeed;
 
-        // compute derived values
+        // Calls the respective helper methods to compute and calculate derived values
         this.volumeTotal = calcVolumeTotal();
         this.volumeAvg = calcVolumeAvg();
         this.speedAvg = calcSpeedAvg();
     }
 
-    // Suggested file format:
-    // [roadVolume data],[roadSpeed data],volumeTotal,volumeAvg,speedAvg
+    /**
+     * Returns this object's data as a comma-separated string for file output.
+     *
+     * @return a CSV-formatted string containing road section data
+     */
     public String getFileData() {
+
+        // Combines all values into CSV format for the return value
         return roadVolume.getFileData() + "," + roadSpeed.getSpeedSensor1() + "," + roadSpeed.getSpeedSensor2() + "," + volumeTotal + "," + volumeAvg + "," + speedAvg;
     }
 
+    /**
+     * Returns the RoadVolume object for this road section.
+     *
+     * @return the road volume object
+     */
     public RoadVolume getRoadVolume() {
         return roadVolume;
     }
 
+    /**
+     * Sets the RoadVolume object for this road section and refreshes derived values.
+     *
+     * @param roadVolume the new road volume object
+     */
     public void setRoadVolume(RoadVolume roadVolume) {
         this.roadVolume = roadVolume;
         // refresh derived values
@@ -35,52 +63,110 @@ public class RoadSection {
         this.volumeAvg = calcVolumeAvg();
     }
 
+    /**
+     * Returns the RoadSpeed object for this road section.
+     *
+     * @return the road speed object
+     */
     public RoadSpeed getRoadSpeed() {
         return roadSpeed;
     }
 
+    /**
+     * Sets the RoadSpeed object for this road section and refreshes derived values.
+     *
+     * @param roadSpeed the new road speed object
+     */
     public void setRoadSpeed(RoadSpeed roadSpeed) {
         this.roadSpeed = roadSpeed;
         // refresh derived values
         this.speedAvg = calcSpeedAvg();
     }
 
+    /**
+     * Returns the total volume for this road section.
+     *
+     * @return the total volume
+     */
     public int getVolumeTotal() {
         return volumeTotal;
     }
 
+    /**
+     * Sets the total volume for this road section.
+     *
+     * @param total the new total volume
+     */
     public void setVolumeTotal(int total) {
         this.volumeTotal = total;
     }
 
+    /**
+     * Returns the average volume for this road section.
+     *
+     * @return the average volume
+     */
     public double getVolumeAvg() {
         return volumeAvg;
     }
 
+    /**
+     * Sets the average volume for this road section.
+     *
+     * @param avg the new average volume
+     */
     public void setVolumeAvg(double avg) {
         this.volumeAvg = avg;
     }
 
+    /**
+     * Returns the average speed for this road section.
+     *
+     * @return the average speed
+     */
     public double getSpeedAvg() {
         return speedAvg;
     }
 
+    /**
+     * Sets the average speed for this road section.
+     *
+     * @param avg the new average speed
+     */
     public void setSpeedAvg(double avg) {
         this.speedAvg = avg;
     }
 
-    // ---- private helpers from UML ----
+    // -------- PRIVATE HELPER METHODS --------
 
+    /**
+     * Calculates the total volume using the four volume sensor readings.
+     *
+     * @return the total volume
+     */
     private int calcVolumeTotal() {
         return roadVolume.getVolumeSensor1() + roadVolume.getVolumeSensor2() + roadVolume.getVolumeSensor3() + roadVolume.getVolumeSensor4();
     }
 
+    /**
+     * Calculates the average volume using the four volume sensor readings.
+     *
+     * @return the average volume
+     */
     private double calcVolumeAvg() {
-        // 4 volume sensors
+
+        // Returns average by dividing the total volume by the amount of sensors (4 sensors)
         return calcVolumeTotal() / 4.0;
     }
 
+    /**
+     * Calculates the average speed using the two speed sensor readings.
+     *
+     * @return the average speed
+     */
     private double calcSpeedAvg() {
+
+        // Returns the average of the two speed sensors
         return (roadSpeed.getSpeedSensor1() + roadSpeed.getSpeedSensor2()) / 2.0;
     }
 }
--- projects/projectTwo/src/main/java/unf/edu/cop3503/RoadSpeed.java
+++ projects/projectTwo/src/main/java/unf/edu/cop3503/RoadSpeed.java
@@ -3,12 +3,24 @@ package unf.edu.cop3503;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+/**
+ * Stores road speed data for a single date and time.
+ * Includes two speed sensor readings.
+ */
 public class RoadSpeed {
     private Date date;
     private String time;
     private double speedSensor1;
     private double speedSensor2;
 
+    /**
+     * Creates a RoadSpeed object with the given date, time, and sensor readings.
+     *
+     * @param date the date of the speed reading
+     * @param time the time of the speed reading
+     * @param speedSensor1 the reading from speed sensor 1
+     * @param speedSensor2 the reading from speed sensor 2
+     */
     public RoadSpeed(Date date, String time, double speedSensor1, double speedSensor2) {
         this.date = date;
         this.time = time;
@@ -16,41 +28,85 @@ public class RoadSpeed {
         this.speedSensor2 = speedSensor2;
     }
 
-    // Suggested file format: date,time,speed1,speed2
+    /**
+     * Returns this object's data as a comma-separated string for file output.
+     *
+     * @return a CSV-formatted string containing the date, time, and speed sensor data
+     */
     public String getFileData() {
         SimpleDateFormat outputDateFormat = new SimpleDateFormat("MM/dd/yyyy");
 
         return outputDateFormat.format(date) + "," + time + "," + speedSensor1 + "," + speedSensor2;
     }
 
+    /**
+     * Returns the date of this speed record.
+     *
+     * @return the date
+     */
     public Date getDate() {
         return date;
     }
 
+    /**
+     * Sets the date of this speed record.
+     *
+     * @param date the new date
+     */
     public void setDate(Date date) {
         this.date = date;
     }
 
+    /**
+     * Returns the time of this speed record.
+     *
+     * @return the time
+     */
     public String getTime() {
         return time;
     }
 
+    /**
+     * Sets the time of this speed record.
+     *
+     * @param time the new time
+     */
     public void setTime(String time) {
         this.time = time;
     }
 
+    /**
+     * Returns the first speed sensor reading.
+     *
+     * @return the first speed sensor reading
+     */
     public double getSpeedSensor1() {
         return speedSensor1;
     }
 
+    /**
+     * Sets the first speed sensor reading.
+     *
+     * @param speedSensor1 the new first speed sensor reading
+     */
     public void setSpeedSensor1(double speedSensor1) {
         this.speedSensor1 = speedSensor1;
     }
 
+    /**
+     * Returns the second speed sensor reading.
+     *
+     * @return the second speed sensor reading
+     */
     public double getSpeedSensor2() {
         return speedSensor2;
     }
 
+    /**
+     * Sets the second speed sensor reading.
+     *
+     * @param speedSensor2 the new second speed sensor reading
+     */
     public void setSpeedSensor2(double speedSensor2) {
         this.speedSensor2 = speedSensor2;
     }
