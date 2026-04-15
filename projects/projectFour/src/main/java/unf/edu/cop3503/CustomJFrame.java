9844f51b14f921a03713aabe40448c3d52a5f240
Albert Simpson <albertsimpson00@gmail.com>
2026-04-14 21:50:41 -0400

    Project 4 - Scaffold project classes and define program structure
    Adds skeleton implementations for CustomJFrame and FileHandler, and updates the main Project4 class with TODOs for future implementation. This establishes the class hierarchy and structure required for the project.

---
+++ projects/projectFour/src/main/java/unf/edu/cop3503/CustomJFrame.java
@@ -0,0 +1,15 @@
+package unf.edu.cop3503;
+
+import javax.swing.*;
+
+public class CustomJFrame extends JFrame {
+    // TODO: implement CustomJFrame and all its variables
+
+    public CustomJFrame(){
+        // TODO: implement this method
+    }
+
+    private class InnerActionListener {
+        // TODO: implement InnerActionListener & ActionPerformed & clearForm inside.
+    }
+}
---
+++ projects/projectFour/src/main/java/unf/edu/cop3503/FileHandler.java
@@ -0,0 +1,17 @@
+package unf.edu.cop3503;
+
+import java.io.FileWriter;
+import java.io.PrintWriter;
+import java.io.IOException;
+
+public class FileHandler{
+    // TODO: implement FileHandler and initialize all variables from UML
+
+    public FileHandler(){
+        // TODO: implement FileHandler
+    }
+
+    public void writeResults(String surveyData){
+        // TODO: implement writeResults
+    }
+}
